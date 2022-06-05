16622d20c4b449c44ba661f1382fe2b49576bbe5
Zarpyk <30746531+Zarpyk@users.noreply.github.com>
2022-06-06 00:42:34 +0200

    Better tutorial

--- TwitchRewardSlideshow/Utilities/InformationChecker.cs
+++ TwitchRewardSlideshow/Utilities/InformationChecker.cs
@@ -29,30 +29,31 @@ namespace TwitchRewardSlideshow.Utilities {
 
         public static void CheckOBSWebSocket(ref AppConfig appConfig) {
             bool accept = false;
-            ImageInputDialog webSocketIpDialog = new("Introduce la contraseña del WebSocket y recomiendo deshabilitar" +
-                                                     " \"Habilitar alertas en la bandeja de sistema\"",
-                                                     "Help_WebSocket.gif", true, true, appConfig.obsInfo.obsPass);
+            const string text = "1. Introduce la contraseña del WebSocket que has configurado en el campo de abajo.\n" +
+                                "2. Recomiendo deshabilitar \"Habilitar alertas en la bandeja de sistema\"";
+            ImageInputDialog webSocketIpDialog = new(text, "Help_WebSocket.gif", true, true, appConfig.obsInfo.obsPass);
             do {
                 if (webSocketIpDialog.ShowDialog() == true) {
                     appConfig.obsInfo.obsPass = webSocketIpDialog.result;
                     accept = true;
                 } else {
-                    webSocketIpDialog = new("Introduce la contraseña del WebSocket y recomiendo deshabilitar" +
-                                            " \"Habilitar alertas en la bandeja de sistema\"",
-                                            "Help_WebSocket.gif", true, true, appConfig.obsInfo.obsPass);
+                    webSocketIpDialog = new(text, "Help_WebSocket.gif", true, true, appConfig.obsInfo.obsPass);
                 }
             } while (!accept);
         }
 
         public static void CheckOBSCreateSource(ref AppConfig appConfig) {
             bool accept = false;
-            ImageInputDialog webSocketIpDialog = new("Introduce el nombre de la galeria de imagenes",
-                                                     "Help_CreateSource.gif", false, true,
+            const string text = "Crea e introduce el nombre de la galeria de imagenes (Nombre exacto)";
+            ImageInputDialog webSocketIpDialog = new(text, "Help_CreateSource.gif", false, true,
                                                      appConfig.obsInfo.gallerySourceName);
             do {
                 if (webSocketIpDialog.ShowDialog() == true) {
                     appConfig.obsInfo.gallerySourceName = webSocketIpDialog.result;
                     accept = true;
+                } else {
+                    webSocketIpDialog = new(text, "Help_CreateSource.gif", false, true,
+                                            appConfig.obsInfo.gallerySourceName);
                 }
             } while (!accept);
         }
@@ -60,12 +61,12 @@ namespace TwitchRewardSlideshow.Utilities {
         public static void CheckOBSConfigureSource(ref AppConfig appConfig) {
             bool accept = false;
             const string text = "1. Introduce el tiempo que quieres que haya entre diapositivas (en ms) " +
-                                "(no hace falta hacerlo en OBS, va por separado).\n" +
-                                "2. **IMPORTANTE**: Necesitas establecer la relación de aspecto a un " +
-                                "valor fijo y no dejarlo en automatico.\n" +
-                                "(Por ejemplo puedes poner el tamaño de los A4, que es de \"595x842\", eso si, " +
-                                "no se recomienda resoluciones demasiado grandes)";
-
+                                "(Solo ponerlo aqui, no hace falta hacerlo en OBS, va por separado).\n" +
+                                "2. **IMPORTANTE**: En la galeria de imagenes, necesitas establecer \n" +
+                                "la relación de aspecto a un valor fijo y no dejarlo en \"Automatico\",\n" +
+                                "como se muestra en el gif de abajo. (Por ejemplo puedes poner el tamaño\n" +
+                                "de los A4, que es de \"595x842\", eso si, no se recomienda resoluciones\n" +
+                                "demasiado grandes)";
             ImageInputDialog webSocketIpDialog = new(text, "Help_ConfigSource.gif", false, true,
                                                      appConfig.obsInfo.slideTimeInMilliseconds.ToString());
             do {
@@ -88,26 +89,35 @@ namespace TwitchRewardSlideshow.Utilities {
                 "que se haya puesto anteriormente en la \"relación de aspecto\" de la galería de imágenes e ir poco \n" +
                 "a poco adaptandolo al croma para que quede bien.";
 
-            //TODO no sale el gif
-            ImageInputDialog webSocketIpDialog =
-                new(text, "Help_FilterSource.gif", false, true, "No hace falta nada :)");
+            ImageInputDialog webSocketIpDialog = new(text, "Help_FilterSource.gif", false, true,
+                                                     "No hace falta nada :) (Me da pereza hacer otro dialogo)");
             webSocketIpDialog.ShowDialog();
         }
 
         public static void CheckFolders() {
+            bool accept = false;
             MessageBox.Show("A continuación, selecciona una carpeta para guardar las imagenes que se van a descargar");
             AppConfig appConfig = App.config.Get<AppConfig>();
             string imageFolder = appConfig.imageFolder;
             FolderPicker dlg = new() {
                 inputPath = Directory.Exists(imageFolder) ? imageFolder : @"C:\"
             };
-            if (dlg.ShowDialog() == true) {
-                appConfig = App.config.Get<AppConfig>();
-                appConfig.imageFolder = dlg.resultPath;
-                appConfig.defaultPosterFolder = Path.Combine(dlg.resultPath, "Default");
-                Directory.CreateDirectory(appConfig.defaultPosterFolder);
-                App.config.Set(appConfig);
-            }
+
+            do {
+                if (dlg.ShowDialog() == true) {
+                    appConfig = App.config.Get<AppConfig>();
+                    appConfig.imageFolder = dlg.resultPath;
+                    appConfig.defaultPosterFolder = Path.Combine(dlg.resultPath, "Default");
+                    Directory.CreateDirectory(appConfig.defaultPosterFolder);
+                    App.config.Set(appConfig);
+                    accept = true;
+                } else {
+                    MessageBox.Show("Selecciona una carpeta para guardar las imagenes que se van a descargar");
+                    dlg = new() {
+                        inputPath = Directory.Exists(imageFolder) ? imageFolder : @"C:\"
+                    };
+                }
+            } while (!accept);
         }
     }
 }
